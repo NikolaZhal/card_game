1dab0c9e119098395d8d1f90a558b92a19db94ff
NikolaZhal <ryazan62nik@yandex.ru>
2024-02-26 15:32:17 +0300

    Card

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Card:
-    def __init__(self):
-        ...
-        
+    def __init__(self, attack, cost):
+        self.attack = attack
+        self.cost = cost
+
     def make_hod(self):
         ...
     
     def get_info(self):
-        ...
+        return {'attack': self.attack, 'cost': self.cost}
