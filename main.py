9270eb7ee2fdcbe76e93ddc266f3be3e61cea063
NikolaZhal <ryazan62nik@yandex.ru>
2024-02-26 15:52:39 +0300

    I fuck pygame

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+
+class Game:
+    def __init__(self):
+        self.first_hand = [[0, 0, 0, 0],
+                           [0, 0, 0, 0]]
+        self.second_hand = [[0, 0, 0, 0],
+                            [0, 0, 0, 0]]
+
+    def stand_card(self):
+        ...
+
+    def next_hod(self, who, position, card, row = 1):
+        if who == 1:
+            self.first_hand[row][position] = card
+        else:
+            self.second_hand[row][position] = card
+
